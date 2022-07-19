cd6b9138064e98f1ff1bb6b9358ffd11ee7b89aa
Manjima-manohar <109478538+Manjima-manohar@users.noreply.github.com>
2022-07-19 10:12:32 +0530

    Update clientpgm.py

--- clientpgm.py
+++ clientpgm.py
@@ -1,4 +1,3 @@
-#client program
 from http.server import requests
 payload = {'payload':'0x38 0x39 0x20 0x35 0x30 0x20 0x37 0x30 0x20 0x34 0x38'}
 r = requests.POST('http://localhost:7878/post', data=json.dumps(payload))
